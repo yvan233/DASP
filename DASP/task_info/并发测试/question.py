e8bda8c9c72371a118a359961e7efe788cceadea
yvan233 <2624507753@qq.com>
2021-07-10 17:19:01 +0800

    dev2

--- DASP/task_info/并发测试/question.py
+++ DASP/task_info/并发测试/question.py
@@ -5,7 +5,7 @@ def taskFunction(self,id,adjDirection,datalist):
     data = [[id]]*len(adjDirection)
     for m in range(10000):
         # self.transmitData(adjDirection, data)
-        # time.sleep(1)
+        time.sleep(1)
         self.syncNode()
         print (m)
     return 0
