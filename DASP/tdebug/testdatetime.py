cdfa20c2a8057d84deb1ef1287acd6acba9b75b0
yvan233 <2624507753@qq.com>
2021-07-26 22:07:35 +0800

    时间同步

--- DASP/tdebug/testdatetime.py
+++ DASP/tdebug/testdatetime.py
@@ -1,10 +1,22 @@
 import time
 from datetime import datetime
+date = datetime.now()
 
-db_time = time.strftime("%Y-%m-%d %H:%M:%S")
+cur_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+print(date, cur_date)
 
-#
+d1 = '2021-07-26 01:44:51.143242'
+d2 = '2021-07-26 01:44:53.163242'
+d3 = '2021-07-26 01:44:54.173242'
+d4 = '2021-07-26 01:44:57.233242'
 
-cur_date = datetime.now().strftime("%Y%m%d")
-zero_time = datetime(int(cur_date[0:4]), int(cur_date[4:6]), int(cur_date[6:8]), 0, 0)
-print((datetime.now()-zero_time))
+d1 = datetime.strptime(d1,"%Y-%m-%d %H:%M:%S.%f")
+d2 = datetime.strptime(d2,"%Y-%m-%d %H:%M:%S.%f")
+d3 = datetime.strptime(d3,"%Y-%m-%d %H:%M:%S.%f")
+d4 = datetime.strptime(d4,"%Y-%m-%d %H:%M:%S.%f")
+theta = (d2-d1+d4-d3)/2
+real_time = d3+theta
+settime = real_time.strftime("%Y-%m-%d %H:%M:%S.%f")
+print (theta,settime)
+a = 1
+# 写法回头参考github，正式一些
