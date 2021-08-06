cb7f69ecb32d2e487a70bc38466939fadefbcf51
yvan233 <2624507753@qq.com>
2021-08-06 16:46:42 +0800

    水泵控制

--- DASP/task_info/PumpControl/question.py
+++ DASP/task_info/PumpControl/question.py
@@ -3,6 +3,7 @@ import pymysql
 from datetime import datetime
 from .db_operations import *
 from .control_functons import *
+import numpy as np
 
 def taskFunction(self, id, adjDirection, datalist):
     if id[:4] == "pump":
@@ -18,6 +19,9 @@ def taskFunction(self, id, adjDirection, datalist):
         vfd_frequency_con = 40.0
         step_length = 20            # transport to hp node or sync to hp node
         step_count = 0
+
+        # 阻塞性地获取水泵path
+        data, path, __ = self.descendantData.get()
         
         while True:
             start_time = datetime.now()
@@ -26,8 +30,8 @@ def taskFunction(self, id, adjDirection, datalist):
                 # 定时开关机
                 cur_weekday = start_time.strftime("%A")        # 当前星期
                 cur_date = datetime.now().strftime("%Y%m%d")
-                auto_open_time = datetime(int(cur_date[0:4]), int(cur_date[4:6]), int(cur_date[6:8]), 0, 0)       # 自动开机时间 
-                auto_close_time = datetime(int(cur_date[0:4]), int(cur_date[4:6]), int(cur_date[6:8]), 23, 59)     # 自动关机时间
+                auto_open_time = datetime(int(cur_date[0:4]), int(cur_date[4:6]), int(cur_date[6:8]), 7, 50)       # 自动开机时间 
+                auto_close_time = datetime(int(cur_date[0:4]), int(cur_date[4:6]), int(cur_date[6:8]), 18, 10)     # 自动关机时间
 
                 if cur_weekday in ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']:      # 工作日
                     if datetime.now() > auto_open_time and datetime.now() <= auto_close_time:    # 启停时间内
@@ -38,13 +42,19 @@ def taskFunction(self, id, adjDirection, datalist):
                         tw_re_list = []
                         tw_sup_list = []
                         hp_state_list = []
-
+                    
                         # VFD states
                         try:
-                            data_vfd_onoff = db_read(cursor, 'pump_vfd', 'pump_onoff_feedback', 0)
-                            data_vfd_frequency = db_read(cursor, 'pump_vfd', 'pump_frequency_feedback', 0)
-                            vfd_onoff = int(data_vfd_onoff[0][3])
-                            vfd_frequency = int(data_vfd_frequency[0][3])
+                            data_vfd_onoff = db_read(cursor, 'pump_vfd_his', 'pump_onoff_feedback', step_length)
+                            data_vfd_frequency = db_read(cursor, 'pump_vfd_his', 'pump_frequency_feedback', step_length)
+                            for p in range(0, step_length):
+                                if data_vfd_onoff[step_length-1-p][3] != 'vfd_onoff_state err':
+                                    vfd_onoff = int(data_vfd_onoff[step_length-1-p][3])
+                                    break
+                            for p in range(0, step_length):
+                                if data_vfd_frequency[step_length-1-p][3] != 'vfd_frequency err':
+                                    vfd_frequency = float(data_vfd_frequency[step_length-1-p][3])
+                                    break
                         except Exception as err1:
                             self.sendDatatoGUI("VFD states reading error: " + str(err1))
                         # IO state lists
@@ -63,7 +73,7 @@ def taskFunction(self, id, adjDirection, datalist):
                             self.sendDatatoGUI("IO states reading error: " + str(err2))
                         # Heatpump states - need data transport(list)
                         # 阻塞性地获取数据
-                        self.sendDataToDescendant('apply heatpump data')
+                        self.sendDataToDescendant('apply heatpump data', path[:])
                         data, __, __ = self.descendantData.get()
                         hp_state_list,tw_sup_list = data
 
@@ -72,30 +82,33 @@ def taskFunction(self, id, adjDirection, datalist):
                         if step_count % step_length == 0:
                             # Control algorithms
                             try:
+                                avg_tw_re = np.mean(tw_re_list)
+                                avg_tw_sup = np.mean(tw_sup_list)
+                                avg_dtw = avg_tw_re - avg_tw_sup
+                                self.sendDatatoGUI('vfd_frequency:{}  avg_tw_re:{} avg_tw_sup:{} avg_dtw:{}'.format(vfd_frequency, avg_tw_re, avg_tw_sup, avg_dtw))
                                 vfd_frequency_con = pump_control_cooling(vfd_frequency, tw_re_list, tw_sup_list)
                                 step_count += 1
                             except Exception as err4:
                                 self.sendDatatoGUI("Control algorithm error: " + str(err4))
-                        
+                        self.sendDatatoGUI('step:{} pump_onoff_setpoint:{}  pump_frequency_setpoint:{}'.format(step, vfd_onoff_con, vfd_frequency_con))
+                        db_operate(cursor, 'pump_vfd_control', db_time, '0x24000208', 'pump_onoff_setpoint', str(vfd_onoff_con), step)
+                        db_operate(cursor, 'pump_vfd_control', db_time, '0x24000216', 'pump_frequency_setpoint', str(vfd_frequency_con), step)
+                        step += 1
+                       
                     else:
                         vfd_onoff_con = 0
                         step_count = 0
-                    
+                        self.sendDatatoGUI('step:{} pump_onoff_setpoint:{}  pump_frequency_setpoint:{}'.format(step, vfd_onoff_con, vfd_frequency_con))
+                        db_operate(cursor, 'pump_vfd_control', db_time, '0x24000208', 'pump_onoff_setpoint', str(vfd_onoff_con), step)
+                        db_operate(cursor, 'pump_vfd_control', db_time, '0x24000216', 'pump_frequency_setpoint', str(vfd_frequency_con), step)
                     # Write db
-                    self.sendDatatoGUI('pump_onoff_setpoint:{}  pump_frequency_setpoint:{}'.format(vfd_onoff_con, vfd_frequency_con))
-                    db_operate(cursor, 'pump_vfd_control', db_time, '0x24000208', 'pump_onoff_setpoint', str(vfd_onoff_con), step)
-                    db_operate(cursor, 'pump_vfd_control', db_time, '0x24000216', 'pump_frequency_setpoint', str(vfd_frequency_con), step)
-            
+
+                     
             except Exception as cerr:
                 self.sendDatatoGUI("Pump control shell error: " + str(cerr))
-            
-            db.commit()
-            step += 1 
 
-            end_time = datetime.now()
-            if (end_time-start_time).seconds < 60:
-                time.sleep(60 - (end_time-start_time).seconds)
-                
+            db.commit()
+                            
             if (datetime.now()-zero_time).days > 0:
                 clear_con_db(db, 2)
                 step = 0
@@ -103,10 +116,15 @@ def taskFunction(self, id, adjDirection, datalist):
                 zero_time = datetime(int(cur_date[0:4]), int(cur_date[4:6]), int(cur_date[6:8]), 0, 0)
             self.sendDatatoGUI("Control shell working!")
 
+            end_time = datetime.now()
+            if (end_time-start_time).seconds < 60:
+                time.sleep(60 - (end_time-start_time).seconds)
+
     elif id[:4] == "heat":
         db = pymysql.connect(host="localhost", user="root", passwd="cfins", db="data", charset='utf8')
         cursor = db.cursor()
         step_length = 20            # transport to hp node or sync to hp node
+        self.sendDataToRoot("send heatpump path")
         while True:
             if not self.rootData.empty():
                 # 非阻塞性地获取数据
@@ -125,8 +143,9 @@ def taskFunction(self, id, adjDirection, datalist):
                         tw_sup_list.append(float(data_tw_sup[step_length-s-1][3]))
                 except Exception as err3:
                     self.sendDatatoGUI("Heatpump states reading error: " + str(err3))
-                # 发送给后代节点时根据path传输，若path为空则进行广播
-                self.sendDataToRoot([hp_state_list,tw_sup_list])
-                self.sendDatatoGUI("热泵节点成功发送数据")
+                else:
+                    # 发送给后代节点时根据path传输，若path为空则进行广播
+                    self.sendDataToRoot([hp_state_list,tw_sup_list])
+                    self.sendDatatoGUI("热泵节点成功发送数据")
             else:
                 time.sleep(0.01)
