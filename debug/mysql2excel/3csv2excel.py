c5b0830780f2f1ad53218d9da06fb042ab36382a
yvan233 <2624507753@qq.com>
2021-08-25 23:48:39 +0800

    csv数据处理

--- debug/mysql2excel/3csv2excel.py
+++ debug/mysql2excel/3csv2excel.py
@@ -3,7 +3,7 @@ import os
 from collections import OrderedDict
 
 if __name__ == "__main__":
-    date = "2021-08-19"
+    date = "2021-08-24"
     dir_path = 'DB_data/{}'.format(date)
     writer = pd.ExcelWriter('{}/{}.xlsx'.format(dir_path, date))
     files= os.listdir(dir_path) 
