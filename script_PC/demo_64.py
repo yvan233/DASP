68605eed4cd0a7f5f886bb4ee30ba38dbc8f7ae1
yvan233 <yanh20@mails.tsinghua.edu.cn>
2022-10-16 16:57:29 +0800

    add 64nodes topology

---
+++ script_PC/demo_64.py
@@ -0,0 +1,26 @@
+# 启动系统并运行分布式算法
+import time 
+import sys
+sys.path.insert(1,".") # 把上一级目录加入搜索路径
+from DASP.module import Node,Moniter
+from DASP.control import ControlMixin
+
+nodeNum = 64  # 节点数量
+startNode = "1" # 起始节点ID
+nodelist = [] # 节点进程列表
+controlMixin = ControlMixin("Pc") # 控制函数集合
+
+# 启动监控脚本
+moniter = Moniter()
+moniter.run()
+
+# 启动节点进程
+for i in range(nodeNum):
+    node = Node(i)
+    nodelist.append(node)
+
+DappName = "ALST"
+print("start task: "+DappName)
+controlMixin.startTask(DappName,startNode)
+
+moniter.wait()
